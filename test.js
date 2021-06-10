ede9f245badc2bed67e6e1014591ebc0efe19162
amancio sassamba <amancio.sassamba@jupiter.co.ao>
2021-06-10 14:57:11 +0100

    Função para o calculo do factorial

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ console.log("Teste 1");
 var lg = "Life is good";
 
 console.log(lg);
+
+function factorial(numero){
+    var fact = 1;
+    for(let i = 2; i <= numero; i++){
+        fact *= i;
+    }
+    return fact;
+}
