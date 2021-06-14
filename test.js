a69254ae37ce262f95759d2da10f7f1e6260dc0b
amancio sassamba <amancio.sassamba@jupiter.co.ao>
2021-06-14 10:59:02 +0100

    Revert "Função para o calculo do factorial"
    This reverts commit ede9f245badc2bed67e6e1014591ebc0efe19162.

--- test.js
+++ test.js
@@ -3,29 +3,3 @@ console.log("Teste 1");
 var lg = "Life is good";
 
 console.log(lg);
-
-function factorial(numero) {
-    var fact = 1;
-    for (let i = 2; i <= numero; i++) {
-        fact *= i;
-    }
-    return fact;
-}
-
-function pow(base, potencia) {
-    var p = 1;
-    for (let i = 1; i <= potencia; i++) {
-        p *= base;
-    }
-    return p;
-}
-
-//Recursion pow
-function recursion_pow(base, exp) {
-    if (exp == 0)
-        return 1;
-    return base * recursion_pow(base, exp - 1);
-}
-
-console.log("mark");
-console.log("other mark");
