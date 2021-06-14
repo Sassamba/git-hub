67ae0f2fbf93a9c866292a3b6fb5ccd46e6c9326
amancio sassamba <amancio.sassamba@jupiter.co.ao>
2021-06-14 10:22:42 +0100

    function recursion_pow

--- test.js
+++ test.js
@@ -4,10 +4,25 @@ var lg = "Life is good";
 
 console.log(lg);
 
-function factorial(numero){
+function factorial(numero) {
     var fact = 1;
-    for(let i = 2; i <= numero; i++){
+    for (let i = 2; i <= numero; i++) {
         fact *= i;
     }
     return fact;
+}
+
+function pow(base, potencia) {
+    var p = 1;
+    for (let i = 1; i <= potencia; i++) {
+        p *= base;
+    }
+    return p;
+}
+
+//Recursion pow
+function recursion_pow(base, exp) {
+    if (exp == 0)
+        return 1;
+    return base * recursion_pow(base, exp - 1);
 }
